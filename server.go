9342bdb60ba4b92550119f3fb04aebb5bc3a2477
shinya yuta <e145742@shinyayuuta-no-MacBook-Pro.local>
2016-10-26 18:50:08 +0900

    server

---
+++ server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+)
+
+type Hello struct {
+	Serve int
+}
+
+func main() {
+	e := echo.New()
+
+	e.GET("/hello", get)
+	e.POST("/world", post)
+	e.PUT("/put", put)
+	e.DELETE("/delete", delete)
+	e.Run(standard.New(":4000"))
+}
+
+func get(c echo.Context) error {
+	text := c.QueryParam("text")
+	name := c.QueryParam("name")
+	return c.String(http.StatusOK, "Hello, World!: "+text+" "+name)
+}
+
+func post(c echo.Context) error {
+	text := c.FormValue("text")
+	name := c.FormValue("name")
+	return c.String(http.StatusOK, "Nice:"+text+" "+name+" !!")
+}
+
+func put(c echo.Context) error {
+	text := c.FormValue("text")
+	return c.String(http.StatusOK, "put:"+text+" !!")
+}
+
+func delete(c echo.Context) error {
+	is_show := c.QueryParam("is_show")
+	return c.String(http.StatusOK, "delete:"+is_show+" !!")
+}
---
+++ tour/exercise-loops-and-functions.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func Sqrt(x float64) float64 {
+	z := float64(1)
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
