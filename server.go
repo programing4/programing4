53ef807d1c2e5f0a43786b24b11f060a379a3cad
kazuminn <e145702@ie.u-ryukyu.ac.jp>
2016-10-26 15:42:25 +0200

    データベースからデータをすべて取ってきて、jsonで返さないといけないから

--- server.go
+++ server.go
@@ -2,21 +2,34 @@ package main
 
 import (
         "database/sql"
+        "encoding/json"
 	"net/http"
         "fmt"
+        "log"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
         _ "github.com/go-sql-driver/mysql"
 )
 
+type Datalice struct {
+        Datas []Data
+}               
+        
+type Data struct {
+        Id int
+        Name string
+        Entry string
+        Is_show bool
+        Create_at string
+}
+ 
 type Hello struct {
 	Serve int
 }
 
 func main() {
 	e := echo.New()
-        _ = initDB()
 
 	e.GET("/hello", get)
 	e.POST("/world", post)
@@ -26,9 +39,35 @@ func main() {
 }
 
 func get(c echo.Context) error {
-	text := c.QueryParam("text")
-	name := c.QueryParam("name")
-	return c.String(http.StatusOK, "Hello, World!: "+text+" "+name)
+        	db := initDB()
+                rows, err := db.Query(
+                `select * from entries order by created_at;`,
+        )   
+
+        var (
+                id int 
+                name string
+                entry string
+                is_show bool
+                create_at string
+        )   
+
+        var a Datalice
+        for rows.Next() {
+                err := rows.Scan(&id,&name,&entry,&is_show,&create_at)
+                if err != nil {
+                        log.Fatal(err)
+                }
+                a.Datas = append(a.Datas, Data{Id: id,Name: name,Entry:entry,Is_show:is_show,Create_at:create_at})
+        }
+
+        b, err := json.Marshal(a)
+        if err != nil {
+                fmt.Println("json err:", err)
+        }
+
+
+	return c.String(http.StatusOK, string(b))
 }
 
 func post(c echo.Context) error {
